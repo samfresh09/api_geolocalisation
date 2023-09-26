1f52d52474020ab666369632c9586745daa29b04
samfresh09 <samtou.gnazouyoufei@ipnetinstitute.com>
2023-09-26 13:29:02 +0000

    calcule de la distance entre 2 points differente

--- controller/position.controller.js
+++ controller/position.controller.js
@@ -1,8 +1,15 @@
 const express = require("express")
-const localisation = require("geolocation")
-const openGeocoder = require('node-open-geocoder');
 
+
+//calcule de distance entre deux position
+const geolib = require("geolib")
+
+//donne les information liee a un lieux
+const openGeocoder = require('node-open-geocoder');
+let geolocation = require('geolocation');
+const { distanceConversion } = require("geolib");
 const router = express.Router()
+
 
 router.get("/position/:lieu", (req, res) => {
     lieu = req.params.lieu
@@ -24,12 +31,43 @@ router.get("/lieu", (req, res) => {
 
     console.log(req);
 
-    // const lieu = openGeocoder()
-    //     .reverse(-8.945406, 38.575078)
-    //     .end((err, res) => {
-    //         console.log(res);
-    //     })
+    const lieu = openGeocoder()
+        .reverse(-8.945406, 38.575078)
+        .end((err, res) => {
+            console.log(res);
+        })
 
+
+})
+
+router.get("/localiser", (req, res) => {
+
+    //calcule de la distance entre deux lieux
+    distance = geolib.getPreciseDistance(
+        //depart
+        {
+            latitude: 51.5203,
+            longitude: 7.49347
+        },
+        //arriver
+        { latitude: "51° 31' N", longitude: "7° 28' E" })
+
+    //renvoi les coordonnes du centre d'un lieu
+    centre = geolib.getCenter([
+        { latitude: 52.516272, longitude: 13.377722 },
+        { latitude: 51.515, longitude: 7.453619 },
+        { latitude: 51.503333, longitude: -0.119722 },
+    ])
+
+    console.log(distance / 1000, 'Km')
+    console.log(centre)
+
+    //renvoi le nom de la ville 
+    const lieu = openGeocoder()
+        .reverse(centre.longitude, centre.latitude)
+        .end((err, res) => {
+            console.log(res.name);
+        })
 
 })
 module.exports = router
